c89e0a5cecb923ec7c10ae311172a627e75183c7
Kevin Siraki <kevsiraki@gmail.com>
2026-03-07 23:04:40 -0800

    Commit

--- hangfire-test/Program.cs
+++ hangfire-test/Program.cs
@@ -1,41 +1,29 @@
 using hangfire_test.Components;
 using Hangfire;
-using Hangfire.MemoryStorage;
+using Hangfire.SqlServer;
 using HangfireBlazorDemo.Jobs;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Razor Components
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Hangfire
 builder.Services.AddHangfire(config =>
-    config.UseMemoryStorage());
+    config.UseSqlServerStorage(
+        builder.Configuration.GetConnectionString("HangfireConnection")));
 
 builder.Services.AddHangfireServer();
 
-// Job service
 builder.Services.AddTransient<MyBackgroundJob>();
 
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
+app.MapStaticAssets();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
+app.UseHangfireDashboard("/hangfire");
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-// Optional dashboard
-app.UseHangfireDashboard("/hangfire");
 
 app.Run();
